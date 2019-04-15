e1766d78210a9e5863fe37bfefeef189f6c88d99
Дмитрий <Dmitriy.Vas.B@gmail.com>
2019-04-15 18:08:48 +0500

    Migrating to gulp@4.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,61 @@
-const gulp = require('gulp');
-const gulpTS = require('gulp-typescript');
-const gulpTSLint = require('gulp-tslint').default;
-const gulpSourcemaps = require('gulp-sourcemaps');
-const gulpNodemon = require('gulp-nodemon');
-const tsLint = require('tslint');
-const del = require('del');
-const path = require('path');
+const gulp = require("gulp");
+const gulpTS = require("gulp-typescript");
+const gulpTSLint = require("gulp-tslint").default;
+const gulpSourcemaps = require("gulp-sourcemaps");
+const gulpNodemon = require("gulp-nodemon");
+const tsLint = require("tslint");
+const del = require("del");
+const path = require("path");
 
-const project = gulpTS.createProject('tsconfig.json');
-const typeCheck = tsLint.Linter.createProgram('tsconfig.json');
+const project = gulpTS.createProject("tsconfig.json");
+const typeCheck = tsLint.Linter.createProgram("tsconfig.json");
 
-gulp.task('default', ['start']);
-
-gulp.task('lint', () => {
-	return gulp.src('./src/**/*.ts')
+gulp.task("lint", gulp.series(function () {
+    return gulp.src("./src/**/*.ts")
 		.pipe(gulpTSLint({
-			configuration: 'tslint.json',
-			formatter: 'verbose',
-			program: typeCheck
+            configuration: "tslint.json",
+            formatter: "verbose",
+            program: typeCheck,
 		}))
-		.pipe(gulpTSLint.report())
-});
+        .pipe(gulpTSLint.report());
+}));
 
-gulp.task('build', ['lint'], () => {
-	del.sync(['./build/**/*.*']);
-	gulp.src('./src/**/*.json')
-		.pipe(gulp.dest('build/'));
-	const tsCompile = gulp.src('./src/**/*.ts')
+gulp.task("clean", gulp.series(function () {
+    return del(["./build/**/*.*"]);
+}));
+
+gulp.task("build", gulp.series("lint", "clean", function (done) {
+    gulp.src("./src/**/*.json")
+        .pipe(gulp.dest("build/"));
+    const tsCompile = gulp.src("./src/**/*.ts")
 		.pipe(gulpSourcemaps.init())
 		.pipe(project());
-	return tsCompile.js
+    tsCompile.js
 		.pipe(gulpSourcemaps.write({
 			sourceRoot: file => path.relative(path.join(file.cwd, file.path),
-				file.base)
+                file.base),
 		}))
-		.pipe(gulp.dest('build/'))
-});
+        .pipe(gulp.dest("build/"));
+    done();
+}));
 
-gulp.task('watch', ['build'], function() {
-	gulp.watch('./src/**/*.ts', ['build'])
-});
+gulp.task("watch", gulp.series("build", function () {
+    return gulp.watch(".", ["build"]);
+}));
 
-gulp.task('start', ['build'], function() {
+gulp.task("serve", gulp.series("watch", function () {
 	return gulpNodemon({
-		script: './index.js',
-		watch: './src'
-	})
-});
+        script: "./index.js",
+        watch: "./",
+    });
+}));
 
-gulp.task('serve', ['watch'], function() {
-	return gulpNodemon({
-		script: './index.js',
-		watch: './src/'
-	})
-});
+gulp.task("start", gulp.series("build", function (done) {
+    gulpNodemon({
+        script: "./index.js",
+        watch: "./",
+    });
+    done();
+}));
+
+gulp.task("default", gulp.series("start"));
